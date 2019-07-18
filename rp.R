c1057309f611e7321acb16d212b000e976c8cba7
Marc Trunjer Kusk Nielsen <marc.mtk@gmail.com>
2019-07-18 15:20:17 +0200

    Update rp.R

--- rp.R
+++ rp.R
@@ -9,17 +9,22 @@ library(glue)
 
 drug_full_names <- tribble(
   ~drug, ~drug_full,
-  "PTZ", "Piperacillin/Tazobactam"
+  "PTZ", "Piperacillin/Tazobactam",
+  "Clari", "Clarithromycin",
+  "PeniG", "Benzylpenicillin"
 )
 
 drug_doses <- tribble(
   ~egfr_range, ~weight_range, ~liver_impairment, ~drug, ~dose, ~admin_form, ~indication,
-  "50-200", "35-80", "Any", "PTZ", "4g x 3", "iv", "Infection",
-  "10-50" , "35-80", "Any", "PTZ", "4g x 2", "iv", "Infection", 
-  "00-10" , "35-80", "Any", "PTZ", "4g x 1", "iv", "Infection",
-  "50-200", "35-80", "Any", "PTZ", "4g x 4", "iv", "Sepsis / Pseudomonas",
-  "10-50" , "35-80", "Any", "PTZ", "4g x 2", "iv", "Sepsis / Pseudomonas",
-  "00-10" , "35-80", "Any", "PTZ", "4g x 1", "iv", "Sepsis / Pseudomonas"
+  "50-200", "35-80" , "Any", "PTZ"  , "4g x 3"    , "iv", "Infection",
+  "10-50" , "35-80" , "Any", "PTZ"  , "4g x 2"    , "iv", "Infection", 
+  "00-10" , "35-80" , "Any", "PTZ"  , "4g x 1"    , "iv", "Infection",
+  "00-10" , "35-80" , "Any", "PTZ"  , "4g x 1"    , "iv", "Sepsis / Pseudomonas",
+  "50-200", "35-80" , "Any", "PTZ"  , "4g x 4"    , "iv", "Sepsis / Pseudomonas",
+  "10-50" , "35-80" , "Any", "PTZ"  , "4g x 2"    , "iv", "Sepsis / Pseudomonas",
+  "30-100", "35-130", "Any", "Clari", "500 mg x 2", "iv / po", "Infection",
+  "50-100", "35-100", "Any", "PeniG", "2 MIE x 4", "iv", "Infection / Sepsis",
+  "50-100", "35-100", "Any", "PeniG", "1 MIE x 4", "iv", "Pneumonia"
 ) %>% 
   mutate(liver_impairment = if_else(liver_impairment == "Any", "TRUE, FALSE", liver_impairment)) %>% 
   separate_rows(liver_impairment, convert = TRUE) %>% 
@@ -57,7 +62,7 @@ print.prescription <- function(x) {
   )
   prescription_args <- c("drug", "suspicion", "egfr", "weight", "liver_impaired")
   missing_args <- prescription_args[prescription_args %notin% names(x)]
-  fully_defined_prescription <- structure(c(x, defaults[missing_args]), class = "prescription")
+  fully_defined_prescription <<- structure(c(x, defaults[missing_args]), class = "prescription")
   text <- resolve_prescription(fully_defined_prescription)
   cat(text)
   invisible(text)
@@ -93,37 +98,68 @@ resolve_prescription <- function(obj) {
 expect_error(rp())
 expect_is(rp("PTZ"), "prescription")
 test_that("PTZ is dosed correctly in a variety of situations", {
-  expect_equal(rp("PTZ")$text, "Rp. iv. Piperacillin/Tazobactam 4g x 3 for Infection")
-  expect_equal(rp("PTZ", egfr = 40)$text,   "Rp. iv. Piperacillin/Tazobactam 4g x 2 for Infection")
-  expect_equal(rp("PTZ", egfr = 5)$text,    "Rp. iv. Piperacillin/Tazobactam 4g x 1 for Infection")
-  expect_equal(rp("PTZ", egfr = 50)$text,   "Rp. iv. Piperacillin/Tazobactam 4g x 3 for Infection")
-  expect_equal(rp("PTZ", weight = 80)$text, "Rp. iv. Piperacillin/Tazobactam 4g x 3 for Infection")
-  expect_equal(rp("PTZ", suspicion = "Pseudomonas")$text, "Rp. iv. Piperacillin/Tazobactam 4g x 4 for Pseudomonas")
+  expect_equal(rp("PTZ"), "Rp. iv. Piperacillin/Tazobactam 4g x 3 for Infection")
+  expect_equal(rp("PTZ", egfr = 40),   "Rp. iv. Piperacillin/Tazobactam 4g x 2 for Infection")
+  expect_equal(rp("PTZ", egfr = 5),    "Rp. iv. Piperacillin/Tazobactam 4g x 1 for Infection")
+  expect_equal(rp("PTZ", egfr = 50),   "Rp. iv. Piperacillin/Tazobactam 4g x 3 for Infection")
+  expect_equal(rp("PTZ", weight = 80), "Rp. iv. Piperacillin/Tazobactam 4g x 3 for Infection")
+  expect_equal(rp("PTZ", suspicion = "Pseudomonas"), "Rp. iv. Piperacillin/Tazobactam 4g x 4 for Pseudomonas")
 })
 
 #test_that("High dose PTZ is correct", rp("PTZ") + suspect_microbe("Pseudomonas aeruginosa") == "Rp. iv. Piperacillin/Tazobactam 4g x 4") # Accepterer at den failer indtil videre. https://github.com/tidyverse/ggplot2/blob/5e8699d9d1463faaa4b489923bc4ef7c51f0afab/R/plot-construction.r
 #test_that("High dose PTZ is correct", rp("PTZ") + suspect_disease("Sepsis") == "Rp. iv. Piperacillin/Tazobactam 4g x 4")
 
-# rp <- function(drug, egfr = 100, weight = 80, liver_impaired = FALSE, suspicion = "Infection") {
-#   is.string(drug)
-#   is.count(egfr); assert_that(egfr >= 0 & egfr < 200)
-#   is.count(weight); assert_that(weight >= 35 & weight < 300)
-#   is.flag(liver_impaired)
-#   is.string(suspicion); assert_that(suspicion %in% drug_doses$indication)
-#   
-#   drug_dose <- filter(drug_doses, 
-#                       drug == drug,       
-#                       egfr_low <= egfr, egfr < egfr_high,
-#                       weight_low < weight, weight <= weight_high,
-#                       liver_impairment == liver_impaired, 
-#                       indication == suspicion) %>% 
-#     left_join(drug_full_names, by = "drug")
-#   assert_that(is.data.frame(drug_dose))
-#   assert_that(nrow(drug_dose) == 1)
-#   
-#   prescription <- structure(list(
-#     text = glue_data(drug_dose, "Rp. {admin_form}. {drug_full} {dose} for {indication}"),
-#     indication = suspicion),
-#     class = "prescription")
-#   return(prescription)
+treatment_aliases <- tribble(
+  ~disease, ~qualifier, ~alias,
+  "Pneumonia", "Community acquired", "CAP",
+  "Pneumonia", "Hospital acquired", "HAP",
+  "Pneumonia", "Ventilator associated", "VAP"
+)
+
+treatments <- tribble(
+  ~disease, ~qualifier, ~severity, ~drug,
+  "Pneumonia", "default", "mild", "PeniG"#,
+  # "Pneumonia", "default", "moderate", "PeniG",
+  # "Pneumonia", "default", "severe", "PTZ + Clari",
+  # "Pneumonia", "atypical", "Any", "Clari"
+)
+
+
+treat <- function(disease) {
+  is.string(disease)
+  structure(list(disease = disease), class = "treatment")
 }
+expect_equal(treat("CAP")$disease, "CAP")
+expect_s3_class(treat("CAP"), "treatment")
+
+exposure <- function(x) {
+  expect(is.string(x), "The exposure should be a string")
+  structure(list(exposure = x), class = "disease_qualifier")
+}
+expect_equal(exposure("Aircondition")$exposure, "Aircondition")
+expect_s3_class(exposure("Aircondition"), "disease_qualifier")
+
+score <- function(x) {
+  expect(is.count(x) | are_equal(x, 0), "Score should be a single whole number 0 or larger")
+  structure(list(score = x #, score_type = "something"
+                 ), class = "disease_qualifier")
+}
+
+"+.treatment" <- function(treatment, disease_qualifier) {
+  assert_that(class(treatment) == "treatment")
+  assert_that(class(disease_qualifier) == "disease_qualifier")
+  assert_that(length(disease_qualifier) == 1)
+  assert_that(names(disease_qualifier) %in% c("exposure", "severity", "score"))
+  assert_that(names(disease_qualifier) %notin% names(treatment))
+  
+  structure(c(treatment, disease_qualifier), class = "treatment")
+}
+treat("CAP") + exposure("Aircondition") 
+expect_error(treat("CAP") + exposure("Aircondition") + exposure("Birds"))
+treat("CAP") + score(5)
+
+expect_equal(treat("CAP")$text, "Rp. iv. benzylpenicillin 1 MIE x 4")
+
+treatment_aliases %>% 
+  filter(alias == "CAP") %>% 
+  glue_data("Treatment for {disease} with qualifiers '{qualifier}' is")
